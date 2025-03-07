ad54f0b314b2a1258403085fb11d7b602ba47ed3
arMustapha5 <abdul.mustapha@amalitech.com>
2025-03-07 09:41:29 +0000

    Solve issue related to chrome user data directory is already in use - 3

--- src/test/java/tests/base/BaseTest.java
+++ src/test/java/tests/base/BaseTest.java
@@ -13,9 +13,9 @@ public class BaseTest {
     @BeforeMethod
     public void setUp() {
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--start-maximized");
-        options.addArguments("--disable-notifications");
-        options.addArguments("--user-data-dir=/tmp/chrome-data"); // Use a unique directory
+//        options.addArguments("--start-maximized");
+//        options.addArguments("--disable-notifications");
+//        options.addArguments("--user-data-dir=/tmp/chrome-data"); // Use a unique directory
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver(options);
 
